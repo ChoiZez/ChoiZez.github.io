4fec754b72ad9fafb95cc8a57472484575a734bb
Max <m.paroshin@mail.ru>
2021-11-09 23:29:57 +0300

    Added navBar and ScrollReveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,64 @@
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close'),
+    dark = document.getElementById('dark')
+
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu'),
+        dark.classList.add('dark')
+    })
+}
+
+if (navClose) {
+  navClose.addEventListener('click', () => {
+      navMenu.classList.remove('show-menu'),
+      dark.classList.remove('dark')
+  })
+}
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+    dark.classList.remove('dark')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop,
+              sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+const darktheme = document.getElementById('article-header')
+
+function scrollLogo(){
+    const scrollY = window.pageYOffset
+    const sectionTop = darktheme.offsetTop
+    if (scrollY>sectionTop-80){
+        document.getElementById('nav-toggle').style.color='rgb(13, 32, 75)'
+    }
+    else{
+        document.getElementById('nav-toggle').style.color='#fff'
+    }
+}
+window.addEventListener('scroll', scrollLogo)
 const sr = ScrollReveal({
   origin: 'top',
   distance:'0px',
